776784665c3e5ed6ed7f4d065a9d889976d6d2d4
Narendra Dharma W. <88047365+raulinns@users.noreply.github.com>
2025-03-19 13:29:28 +0700

    Create PolaAneh.c

---
+++ s2/IF1210 Algoritma dan Pemrograman 1/praktikum-C-language/praktikum-1/prapraktikum/PolaAneh.c
@@ -0,0 +1,43 @@
+// Kode ini tidak sesuai dengan spesifikasi yang diinginkan, seharusnya kode dapat berjalan tanpa array atau matriks
+// Apabila ingin melihat solusi yang lebih sederhana dapat dilihat pada https://github.com/nicholaswisee/Praktikum-IF/
+#include <stdio.h>
+
+int main(){
+    int N;
+    do
+    {
+        scanf("%d", &N);
+    } while (!((1 <= N && N <= 25) && (N % 2 == 1)));
+    char code[N][N];
+    int half = N / 2;
+    int p = 0, q = half;
+
+    for (int i = 0; i <= half; i++) {
+        for (int j = 0; j < N; j++) {
+            code[i][j] = '.';
+        }
+        code[i][half - p] = '*';
+        code[i][half + p] = '*';
+        if (p < half) {
+            p++;
+        }
+    }
+    p = 0;
+    for (int i = (N - 1); i > half; i--) {
+        for (int j = 0; j < N; j++) {
+            code[i][j] = '.';
+        }
+        code[i][half - p] = '*';
+        code[i][half + p] = '*';
+        if (p >= 0) {
+            p++;
+        }
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%c", code[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
