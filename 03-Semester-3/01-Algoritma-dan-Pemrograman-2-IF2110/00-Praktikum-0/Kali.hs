14d242e9898d91ee64f52f797c8fd913d0a6689d
raulin <andradaffamarpaung@gmail.com>
2025-09-05 20:25:49 +0700

    add praktikum-0 100

---
+++ 03-Semester-3/01-Algoritma-dan-Pemrograman-2-IF2110/00-Praktikum-0/Kali.hs
@@ -0,0 +1,9 @@
+-- HEADER
+module Kali where
+
+-- DEFINISI DAN SPESIFIKASI
+kali::Int -> Int -> Int
+-- Mengalikan dua buah bilangan integer a dan b
+
+-- REALISASI
+kali a b = a * b
---
+++ 03-Semester-3/01-Algoritma-dan-Pemrograman-2-IF2110/00-Praktikum-0/Pangkat2.hs
@@ -0,0 +1,9 @@
+-- HEADER
+module Pangkat2 where
+
+-- DEFINISI DAN SPESIFIKASI
+pangkat2::Int -> Int
+-- Menerima bilangan a dan menghasilkan a^2
+
+-- REALISASI
+pangkat2 a = a^2
